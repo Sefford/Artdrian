e5ecd5725dfaf327c86e32d67dbfc67e3dda0afe
Saul Diaz <sefford@gmail.com>
2024-11-04 23:09:39 +0100

    Improves deserializer

--- app/src/main/java/com/sefford/artdrian/data/dto/deserializers/DateDeserializer.kt
+++ app/src/main/java/com/sefford/artdrian/data/dto/deserializers/DateDeserializer.kt
@@ -1,14 +1,25 @@
 package com.sefford.artdrian.data.dto.deserializers
 
+import android.os.Build
+import androidx.annotation.RequiresApi
+import kotlinx.datetime.Instant
+import kotlinx.datetime.LocalDateTime
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toInstant
+import kotlinx.datetime.toLocalDateTime
 import kotlinx.serialization.KSerializer
 import kotlinx.serialization.descriptors.PrimitiveKind
 import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.descriptors.SerialDescriptor
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
-import java.util.Date
 
-object DateDeserializer : KSerializer<Date> {
-    override val descriptor = PrimitiveSerialDescriptor("Date", PrimitiveKind.LONG)
-    override fun serialize(encoder: Encoder, value: Date) = encoder.encodeLong(value.time)
-    override fun deserialize(decoder: Decoder): Date = Date(decoder.decodeLong())
+object DateDeserializer : KSerializer<LocalDateTime> {
+    override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("LocalDateTime", PrimitiveKind.STRING)
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun serialize(encoder: Encoder, value: LocalDateTime) =
+        encoder.encodeString(value.toInstant(TimeZone.UTC).toString())
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun deserialize(decoder: Decoder): LocalDateTime = Instant.parse(decoder.decodeString()).toLocalDateTime(TimeZone.UTC)
 }
--- app/src/main/java/com/sefford/artdrian/data/dto/deserializers/WallpaperResponseDeserializer.kt
+++ app/src/main/java/com/sefford/artdrian/data/dto/deserializers/WallpaperResponseDeserializer.kt
@@ -2,20 +2,27 @@ package com.sefford.artdrian.data.dto.deserializers
 
 import com.sefford.artdrian.data.dto.MetadataDto
 import com.sefford.artdrian.data.dto.WallpaperResponse
+import com.sefford.artdrian.model.Wallpaper
 import kotlinx.serialization.KSerializer
+import kotlinx.serialization.builtins.ListSerializer
 import kotlinx.serialization.descriptors.PrimitiveKind
 import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
 import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.descriptors.buildClassSerialDescriptor
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
 import kotlinx.serialization.json.JsonDecoder
+import kotlinx.serialization.json.decodeFromJsonElement
 import kotlinx.serialization.json.jsonArray
 import kotlinx.serialization.json.jsonObject
 
 class WallpaperResponseDeserializer : KSerializer<WallpaperResponse> {
 
-    override val descriptor: SerialDescriptor
-        get() = PrimitiveSerialDescriptor("WallpaperResponse", PrimitiveKind.STRING)
+    override val descriptor: SerialDescriptor =
+        buildClassSerialDescriptor("Response") {
+            element("wallpapers", ListSerializer(MetadataDto.serializer()).descriptor)
+        }
+
 
     override fun deserialize(decoder: Decoder): WallpaperResponse {
         val jsonDecoder = (decoder as JsonDecoder)
@@ -24,7 +31,7 @@ class WallpaperResponseDeserializer : KSerializer<WallpaperResponse> {
             jsonObject["pageProps"]!!
                 .jsonObject["wallpapers"]!!
                 .jsonArray
-                .map { e -> jsonDecoder.json.decodeFromJsonElement(MetadataDto.serializer(), e) }
+                .map { e -> jsonDecoder.json.decodeFromJsonElement(e) }
         )
     }
 
