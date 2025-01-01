6359cd84bd3e2837269ca6e0d4287c5dac8c1667
Saúl Díaz <sefford@gmail.com>
2025-01-01 19:52:02 +0100

    ART-36 Removes phased out tests

--- app/src/test/java/com/sefford/artdrian/datasources/FakeWallpaperApi.kt
+++
@@ -1,9 +0,0 @@
-package com.sefford.artdrian.datasources
-
-import com.sefford.artdrian.data.dto.WallpaperResponse
-
-class FakeWallpaperApi(private val response: () -> WallpaperResponse) : WallpaperApi {
-
-    override suspend fun getAllMetadata(): WallpaperResponse = response()
-
-}
