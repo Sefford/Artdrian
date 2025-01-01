415c530c065bb6b2b71782b58e09fc31dc8bb262
Saúl Díaz <sefford@gmail.com>
2025-01-01 19:52:02 +0100

    ART-36 Completes Deserializer Tests

--- app/src/test/java/com/sefford/artdrian/data/dto/deserializers/WallpapersResponseDeserializerTest.kt
+++ app/src/test/java/com/sefford/artdrian/data/dto/deserializers/WallpapersResponseDeserializerTest.kt
@@ -2,12 +2,11 @@ package com.sefford.artdrian.data.dto.deserializers
 
 import com.sefford.artdrian.test.Resources
 import com.karumi.kotlinsnapshot.matchWithSnapshot
-import com.sefford.artdrian.data.dto.WallpaperResponse
 import com.sefford.artdrian.data.dto.WallpapersResponse
 import kotlinx.serialization.json.Json
 import org.junit.jupiter.api.Test
 
-class WallpaperResponseDeserializerTest : Resources {
+class WallpapersResponseDeserializerTest : Resources {
 
     private val json = Json {
         ignoreUnknownKeys = true
@@ -16,7 +15,7 @@ class WallpaperResponseDeserializerTest : Resources {
 
     @Test
     fun deserialize() {
-        val response = json.decodeFromString<WallpaperResponse>("single-wallpaper-response.json".asResponse())
+        val response = json.decodeFromString<WallpapersResponse>("single-wallpaper-list-response.json".asResponse())
 
         response.matchWithSnapshot()
     }
