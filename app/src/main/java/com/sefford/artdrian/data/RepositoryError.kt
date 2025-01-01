18ca465efddd4e0c3baaf69d2c98e8f4383ab52d
Saúl Díaz <sefford@gmail.com>
2025-01-01 19:52:02 +0100

    ART-36 Improves on Data Modelling

--- app/src/main/java/com/sefford/artdrian/data/RepositoryError.kt
+++
@@ -1,6 +0,0 @@
-package com.sefford.artdrian.data
-
-sealed class RepositoryError {
-    class NotFound(val id: String = "") : RepositoryError()
-    class NetworkingError(val status: Int = 0) : RepositoryError()
-}
