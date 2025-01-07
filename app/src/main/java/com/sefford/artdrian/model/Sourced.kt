a9f95fa0bc4fee123373d6f8adecf6e98a27faa2
Saúl Díaz <sefford@gmail.com>
2025-01-07 16:26:59 +0100

    ART-48 Fixed a small bug that was looping saves to the database

--- app/src/main/java/com/sefford/artdrian/model/Sourced.kt
+++ app/src/main/java/com/sefford/artdrian/model/Sourced.kt
@@ -9,11 +9,19 @@ interface Sourced {
     object Local: Sourced {
         override val local: Boolean = true
         override val network: Boolean = false
+
+        override fun toString(): String {
+            return "Source: Local"
+        }
     }
 
     object Network: Sourced {
         override val local: Boolean = false
         override val network: Boolean = true
+
+        override fun toString(): String {
+            return "Source: Network"
+        }
     }
 }
 
