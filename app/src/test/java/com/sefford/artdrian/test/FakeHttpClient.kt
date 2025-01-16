60bb627350d94e69e8f7c26cf0fc346727a1d98e
Saúl Díaz <sefford@gmail.com>
2025-01-16 22:54:53 +0100

    ART-59 Added Head function to Fake HttpClient

--- app/src/test/java/com/sefford/artdrian/test/FakeHttpClient.kt
+++ app/src/test/java/com/sefford/artdrian/test/FakeHttpClient.kt
@@ -2,6 +2,7 @@ package com.sefford.artdrian.test
 
 import com.sefford.artdrian.common.data.network.HttpClient
 import io.ktor.client.request.get
+import io.ktor.client.request.head
 import io.ktor.client.statement.HttpResponse
 import io.ktor.client.HttpClient as KtorClient
 
@@ -10,4 +11,6 @@ class FakeHttpClient(
     private val behavior: (String) -> HttpResponse? = { null }
 ): HttpClient {
     override suspend fun get(url: String): HttpResponse = behavior(url) ?: client.get(url)
+
+    override suspend fun head(url: String): HttpResponse = behavior(url) ?: client.head(url)
 }
