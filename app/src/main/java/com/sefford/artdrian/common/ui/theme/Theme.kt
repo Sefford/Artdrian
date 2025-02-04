334356d83e1e575e1005cdcbd3b2c3ff7e44fa91
Saúl Díaz <sefford@gmail.com>
2025-02-04 23:25:01 +0100

    ART-74 Fixes status bar colors

--- app/src/main/java/com/sefford/artdrian/common/ui/theme/Theme.kt
+++ app/src/main/java/com/sefford/artdrian/common/ui/theme/Theme.kt
@@ -28,13 +28,12 @@ import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
 
 val darkTheme: ColorScheme = darkColorScheme(
     primary = Color(0xFFBB86FC),
@@ -132,11 +131,14 @@ fun ArtdrianTheme(
     }
 
     // Update the status bar color based on the theme
-    val view = LocalView.current
-    if (!view.isInEditMode) {
-        SideEffect {
-            (view.context as Activity).window.statusBarColor = colorScheme.primary.toArgb()
-            ViewCompat.getWindowInsetsController(view)?.isAppearanceLightStatusBars = darkMode
+    val activity = LocalView.current.context as? Activity
+    SideEffect {
+        activity?.window?.apply {
+            // Set Status Bar Color
+            WindowCompat.getInsetsController(this, decorView).isAppearanceLightStatusBars = !darkMode
+
+            // Set Navigation Bar Color
+            WindowCompat.getInsetsController(this, decorView).isAppearanceLightNavigationBars = !darkMode
         }
     }
 
