5ea7b581054a9e123dcad9db15fbf3f9e62e1838
Saúl Díaz <sefford@gmail.com>
2025-02-05 00:44:54 +0100

    ART-78 Prepares Wallpaper List ViewModel for injection

---
+++ app/src/main/java/com/sefford/artdrian/wallpapers/ui/list/di/WallpaperListModule.kt
@@ -0,0 +1,9 @@
+package com.sefford.artdrian.wallpapers.ui.list.di
+
+import androidx.navigation.NavHostController
+import dagger.Module
+
+@Module
+class WallpaperListModule(private val navigation: NavHostController) {
+
+}
